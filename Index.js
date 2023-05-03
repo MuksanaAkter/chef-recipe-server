22d16eb453dedd5eb2cdc73213af21772a66fb27
muksana akter <muksanaakter3@gmail.com>
2023-05-03 17:57:20 +0600

    first commit

---
+++ Index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port =process.env.PORT || 3000;
+
+const chef = require('./data/Chef.json');
+
+app.get('/', (req, res) => {
+  res.send('Chef comming')
+})
+
+app.get('/chef', (req, res) => {
+  res.send(chef);
+})
+
+app.get('/chef/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedchef = chef.find(n => n._id === id);
+  res.send(selectedchef)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
